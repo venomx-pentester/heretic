5600b8ee29baca95aeeac64e70bd710d352e52e5
cpagac <coleman.pagac@drake.edu>
2026-02-18 19:34:57 -0600

    fix: remove unused QuantizationMethod import

--- src/heretic/main.py
+++ src/heretic/main.py
@@ -36,7 +36,7 @@ from questionary import Choice
 from rich.traceback import install
 
 from .analyzer import Analyzer
-from .config import QuantizationMethod, Settings
+from .config import Settings
 from .evaluator import Evaluator
 from .model import AbliterationParameters, Model, get_model_class
 from .utils import (
